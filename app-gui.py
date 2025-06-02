fecd1d7d948a8a8a055168982a0880932497b78d
Mohit <mohit.ksharma2305@gmail.com>
2025-06-02 18:48:03 +0100

    Add files via upload

---
+++ app-gui.py
@@ -0,0 +1,310 @@
+from Detector import main_app
+from create_classifier import train_classifer
+from create_dataset import start_capture
+import tkinter as tk
+from tkinter import font as tkfont
+from tkinter import messagebox,PhotoImage
+import cv2
+import os
+import numpy as np
+import tensorflow.compat.v1 as tf
+import sys
+import pytesseract as pt
+import PIL
+from PIL import Image, ImageEnhance, ImageFilter
+#from PIL import ImageTk, Image
+#from gender_prediction import emotion,ageAndgender
+names = set()
+
+
+class MainUI(tk.Tk):
+
+    def __init__(self, *args, **kwargs):
+        tk.Tk.__init__(self, *args, **kwargs)
+        global names
+        with open("nameslist.txt", "r") as f:
+            x = f.read()
+            z = x.rstrip().split(" ")
+            for i in z:
+                names.add(i)
+        self.title_font = tkfont.Font(family='Helvetica', size=16, weight="bold")
+        self.title("Face Recognizer")
+        self.resizable(False, False)
+        self.geometry("500x250")
+        self.protocol("WM_DELETE_WINDOW", self.on_closing)
+        self.active_name = None
+        container = tk.Frame(self)
+        container.grid(sticky="nsew")
+        container.grid_rowconfigure(0, weight=1)
+        container.grid_columnconfigure(0, weight=1)
+        self.frames = {}
+        for F in (StartPage, PageOne, PageTwo, PageThree, PageFour):
+            page_name = F.__name__
+            frame = F(parent=container, controller=self)
+            self.frames[page_name] = frame
+            frame.grid(row=0, column=0, sticky="nsew")
+        self.show_frame("StartPage")
+
+    def show_frame(self, page_name):
+            frame = self.frames[page_name]
+            frame.tkraise()
+
+    def on_closing(self):
+
+        if messagebox.askokcancel("Quit", "Are you sure?"):
+            global names
+            f =  open("nameslist.txt", "a+")
+            for i in names:
+                    f.write(i+" ")
+            self.destroy()
+
+
+class StartPage(tk.Frame):
+
+        def __init__(self, parent, controller):
+            tk.Frame.__init__(self, parent)
+            self.controller = controller
+            #load = Image.open("homepagepic.png")
+            #load = load.resize((250, 250), Image.ANTIALIAS)
+            render = PhotoImage(file='homepagepic.png')
+            img = tk.Label(self, image=render)
+            img.image = render
+            img.grid(row=0, column=1, rowspan=4, sticky="nsew")
+            label = tk.Label(self, text="        Home Page        ", font=self.controller.title_font,fg="#263942")
+            label.grid(row=0, sticky="ew")
+            button1 = tk.Button(self, text="   Add a User  ", fg="#ffffff", bg="#263942",command=lambda: self.controller.show_frame("PageOne"))
+            button2 = tk.Button(self, text="   Check a User  ", fg="#ffffff", bg="#263942",command=lambda: self.controller.show_frame("PageTwo"))
+            button3 = tk.Button(self, text="Quit", fg="#263942", bg="#ffffff", command=self.on_closing)
+            button1.grid(row=1, column=0, ipady=3, ipadx=7)
+            button2.grid(row=2, column=0, ipady=3, ipadx=2)
+            button3.grid(row=3, column=0, ipady=3, ipadx=32)
+
+
+        def on_closing(self):
+            if messagebox.askokcancel("Quit", "Are you sure?"):
+                global names
+                with open("nameslist.txt", "w") as f:
+                    for i in names:
+                        f.write(i + " ")
+                self.controller.destroy()
+
+
+class PageOne(tk.Frame):
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        tk.Label(self, text="Enter the name", fg="#263942", font='Helvetica 12 bold').grid(row=0, column=0, pady=10, padx=5)
+        self.user_name = tk.Entry(self, borderwidth=3, bg="lightgrey", font='Helvetica 11')
+        self.user_name.grid(row=0, column=1, pady=10, padx=10)
+        self.buttoncanc = tk.Button(self, text="Cancel", bg="#ffffff", fg="#263942", command=lambda: controller.show_frame("StartPage"))
+        self.buttonext = tk.Button(self, text="Next", fg="#ffffff", bg="#263942", command=self.start_training)
+        self.buttoncard = tk.Button(self, text= "Read Card", fg='#ffffff', bg="#263942", command=self.id_card)
+        self.buttoncanc.grid(row=1, column=0, pady=10, ipadx=5, ipady=4)
+        self.buttonext.grid(row=1, column=1, pady=10, ipadx=5, ipady=4)
+        self.buttoncard.grid(row=1, column=2, pady=10, ipadx=5, ipady=4)
+    def start_training(self):
+        global names
+        if self.user_name.get() == "None":
+            messagebox.showerror("Error", "Name cannot be 'None'")
+            return
+        elif self.user_name.get() in names:
+            messagebox.showerror("Error", "User already exists!")
+            return
+        elif len(self.user_name.get()) == 0:
+            messagebox.showerror("Error", "Name cannot be empty!")
+            return
+        name = self.user_name.get()
+        names.add(name)
+        self.controller.active_name = name
+        self.controller.frames["PageTwo"].refresh_names()
+        self.controller.show_frame("PageThree")
+    def id_card(self):
+        sys.path.append('..')
+        
+        from utils import label_map_util
+        from utils import visualization_utils as vis_util
+        
+        MODEL_NAME='model'
+        
+        CWD_PATH = os.getcwd()
+        
+        PATH_TO_CKPT = os.path.join(CWD_PATH, MODEL_NAME,'frozen_inference_graph.pb')
+        
+        PATH_TO_LABELS = os.path.join(CWD_PATH,'data','labelmap.pbtxt')
+        
+        NUM_CLASSES = 1
+        
+        label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+        categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+        category_index = label_map_util.create_category_index(categories)
+        
+        detection_graph = tf.Graph()
+        with detection_graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
+                
+            sess = tf.Session(graph=detection_graph)
+            
+        self.image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+        
+        self.detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+        
+        self.detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+        self.detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+        
+        self.num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+        
+        self.video = cv2.VideoCapture(0)
+        self.ret = self.video.set(3,1280)
+        self.ret = self.video.set(4,720)
+        
+        self.img_counter = 0
+        
+        cv2.namedWindow("Identity Card Detection")
+        
+        while(True):
+            self.ret, self.frame = self.video.read()
+            self.frame_expanded = np.expand_dims(self.frame, axis=0)
+            (boxes, scores, classes, num) = sess.run(
+                [self.detection_boxes, self.detection_scores, self.detection_classes, self.num_detections],
+                feed_dict={self.image_tensor: self.frame_expanded})
+            vis_util.visualize_boxes_and_labels_on_image_array(
+                self.frame,
+                np.squeeze(boxes),
+                np.squeeze(classes).astype(np.int32),
+                np.squeeze(scores),
+                category_index,
+                use_normalized_coordinates=True,
+                line_thickness=8,
+                min_score_thresh=0.60)
+            if not self.ret:
+                print("failed to grab frame")
+                break
+            cv2.imshow("Identity Card Detection", self.frame)
+
+            self.k = cv2.waitKey(1)  
+            if self.k%256 == 27:
+                print("Escape hit, closing...")
+                break
+            elif self.k%256 == 32:
+                self.img_name = "opencv_frame_{}.png".format(self.img_counter)
+                cv2.imwrite(self.img_name, self.frame)
+                print("{} written!".format(self.img_name))
+                self.img_counter += 1
+        self.video.release()
+        cv2.destroyAllWindows()
+    
+        pt.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+        self.file= 'opencv_frame_0.png'
+        self.image= Image.open(self.file)
+        self.enhancer1 = ImageEnhance.Sharpness(self.image)
+        self.enhancer2 = ImageEnhance.Contrast(self.image)
+        self.img_edit = self.enhancer1.enhance(20.0)
+        self.img_edit = self.enhancer2.enhance(1.5)
+        self.img_edit.save("edited_image.png")
+
+        self.text = pt.image_to_string(self.img_edit)
+        with open('2.txt','w') as f:
+            f.write(self.text)
+        f.close()
+        
+        file = open("2.txt", "r")
+        file2 = open("3.txt", "w")
+        txt = file.read()
+        wrd = txt.split()
+        s = ' '.join(wrd)
+        file2.write(s)
+
+class PageTwo(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        global names
+        self.controller = controller
+        tk.Label(self, text="Select user", fg="#263942", font='Helvetica 12 bold').grid(row=0, column=0, padx=10, pady=10)
+        self.buttoncanc = tk.Button(self, text="Cancel", command=lambda: controller.show_frame("StartPage"), bg="#ffffff", fg="#263942")
+        self.menuvar = tk.StringVar(self)
+        self.dropdown = tk.OptionMenu(self, self.menuvar, *names)
+        self.dropdown.config(bg="lightgrey")
+        self.dropdown["menu"].config(bg="lightgrey")
+        self.buttonext = tk.Button(self, text="Next", command=self.nextfoo, fg="#ffffff", bg="#263942")
+        self.dropdown.grid(row=0, column=1, ipadx=8, padx=10, pady=10)
+        self.buttoncanc.grid(row=1, ipadx=5, ipady=4, column=0, pady=10)
+        self.buttonext.grid(row=1, ipadx=5, ipady=4, column=1, pady=10)
+
+    def nextfoo(self):
+        if self.menuvar.get() == "None":
+            messagebox.showerror("ERROR", "Name cannot be 'None'")
+            return
+        self.controller.active_name = self.menuvar.get()
+        self.controller.show_frame("PageFour")
+
+    def refresh_names(self):
+        global names
+        self.menuvar.set('')
+        self.dropdown['menu'].delete(0, 'end')
+        for name in names:
+            self.dropdown['menu'].add_command(label=name, command=tk._setit(self.menuvar, name))
+
+class PageThree(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+        self.numimglabel = tk.Label(self, text="Number of images captured = 0", font='Helvetica 12 bold', fg="#263942")
+        self.numimglabel.grid(row=0, column=0, columnspan=2, sticky="ew", pady=10)
+        self.capturebutton = tk.Button(self, text="Capture Data Set", fg="#ffffff", bg="#263942", command=self.capimg)
+        self.trainbutton = tk.Button(self, text="Train The Model", fg="#ffffff", bg="#263942",command=self.trainmodel)
+        self.capturebutton.grid(row=1, column=0, ipadx=5, ipady=4, padx=10, pady=20)
+        self.trainbutton.grid(row=1, column=1, ipadx=5, ipady=4, padx=10, pady=20)
+
+    def capimg(self):
+        self.numimglabel.config(text=str("Captured Images = 0 "))
+        messagebox.showinfo("INSTRUCTIONS", "We will Capture 300 pic of your Face.")
+        x = start_capture(self.controller.active_name)
+        self.controller.num_of_images = x
+        self.numimglabel.config(text=str("Number of images captured = "+str(x)))
+
+    def trainmodel(self):
+        if self.controller.num_of_images < 300:
+            messagebox.showerror("ERROR", "No enough Data, Capture at least 300 images!")
+            return
+        train_classifer(self.controller.active_name)
+        messagebox.showinfo("SUCCESS", "The modele has been successfully trained!")
+        self.controller.show_frame("PageFour")
+
+
+class PageFour(tk.Frame):
+
+    def __init__(self, parent, controller):
+        tk.Frame.__init__(self, parent)
+        self.controller = controller
+
+        label = tk.Label(self, text="Face Recognition", font='Helvetica 16 bold')
+        label.grid(row=0,column=0, sticky="ew")
+        button1 = tk.Button(self, text="Face Recognition", command=self.openwebcam, fg="#ffffff", bg="#263942")
+        #button2 = tk.Button(self, text="Emotion Detection", command=self.emot, fg="#ffffff", bg="#263942")
+        #button3 = tk.Button(self, text="Gender and Age Prediction", command=self.gender_age_pred, fg="#ffffff", bg="#263942")
+        button4 = tk.Button(self, text="Go to Home Page", command=lambda: self.controller.show_frame("StartPage"), bg="#ffffff", fg="#263942")
+        button1.grid(row=1,column=0, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+        #button2.grid(row=1,column=1, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+        #button3.grid(row=2,column=0, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+        button4.grid(row=1,column=1, sticky="ew", ipadx=5, ipady=4, padx=10, pady=10)
+
+    def openwebcam(self):
+        main_app(self.controller.active_name)
+    #def gender_age_pred(self):
+     #  ageAndgender()
+    #def emot(self):
+     #   emotion()
+
+
+        
+
+app = MainUI()
+app.iconphoto(False, tk.PhotoImage(file='icon.ico'))
+app.mainloop()
+
---
+++ id_card_detection_camera.py
@@ -0,0 +1,143 @@
+
+# Import packages
+import os
+import cv2
+import numpy as np
+import tensorflow.compat.v1 as tf
+import sys
+import pytesseract as pt
+import PIL
+from PIL import Image, ImageEnhance, ImageFilter
+
+# This is needed since the notebook is stored in the object_detection folder.
+sys.path.append("..")
+
+# Import utilites
+from utils import label_map_util
+from utils import visualization_utils as vis_util
+
+# Name of the directory containing the object detection module we're using
+MODEL_NAME = 'model'
+
+# Grab path to current working directory
+CWD_PATH = os.getcwd()
+
+# Path to frozen detection graph .pb file, which contains the model that is used
+# for object detection.
+PATH_TO_CKPT = os.path.join(CWD_PATH,MODEL_NAME,'frozen_inference_graph.pb')
+
+# Path to label map file
+PATH_TO_LABELS = os.path.join(CWD_PATH,'data','labelmap.pbtxt')
+
+# Number of classes the object detector can identify
+NUM_CLASSES = 1
+
+## Load the label map.
+# Label maps map indices to category names, so that when our convolution
+# network predicts `5`, we know that this corresponds to `king`.
+# Here we use internal utility functions, but anything that returns a
+# dictionary mapping integers to appropriate string labels would be fine
+label_map = label_map_util.load_labelmap(PATH_TO_LABELS)
+categories = label_map_util.convert_label_map_to_categories(label_map, max_num_classes=NUM_CLASSES, use_display_name=True)
+category_index = label_map_util.create_category_index(categories)
+
+# Load the Tensorflow model into memory.
+detection_graph = tf.Graph()
+with detection_graph.as_default():
+    od_graph_def = tf.GraphDef()
+    with tf.io.gfile.GFile(PATH_TO_CKPT, 'rb') as fid:
+        serialized_graph = fid.read()
+        od_graph_def.ParseFromString(serialized_graph)
+        tf.import_graph_def(od_graph_def, name='')
+
+    sess = tf.Session(graph=detection_graph)
+
+
+# Define input and output tensors (i.e. data) for the object detection classifier
+
+# Input tensor is the image
+image_tensor = detection_graph.get_tensor_by_name('image_tensor:0')
+
+# Output tensors are the detection boxes, scores, and classes
+# Each box represents a part of the image where a particular object was detected
+detection_boxes = detection_graph.get_tensor_by_name('detection_boxes:0')
+
+# Each score represents level of confidence for each of the objects.
+# The score is shown on the result image, together with the class label.
+detection_scores = detection_graph.get_tensor_by_name('detection_scores:0')
+detection_classes = detection_graph.get_tensor_by_name('detection_classes:0')
+
+# Number of objects detected
+num_detections = detection_graph.get_tensor_by_name('num_detections:0')
+
+# Initialize webcam feed
+video = cv2.VideoCapture(0)
+ret = video.set(3,1280)
+ret = video.set(4,720)
+
+img_counter = 0
+
+cv2.namedWindow("test")
+
+while(True):
+
+    # Acquire frame and expand frame dimensions to have shape: [1, None, None, 3]
+    # i.e. a single-column array, where each item in the column has the pixel RGB value
+    ret, frame = video.read()
+    frame_expanded = np.expand_dims(frame, axis=0)
+    
+    # Perform the actual detection by running the model with the image as input
+    (boxes, scores, classes, num) = sess.run(
+        [detection_boxes, detection_scores, detection_classes, num_detections],
+        feed_dict={image_tensor: frame_expanded})
+
+    # Draw the results of the detection (aka 'visulaize the results')
+    vis_util.visualize_boxes_and_labels_on_image_array(
+        frame,
+        np.squeeze(boxes),
+        np.squeeze(classes).astype(np.int32),
+        np.squeeze(scores),
+        category_index,
+        use_normalized_coordinates=True,
+        line_thickness=8,
+        min_score_thresh=0.60)
+    
+    # All the results have been drawn on the frame, so it's time to display it.
+    if not ret:
+        print("failed to grab frame")
+        break
+    cv2.imshow("test", frame)
+
+    k = cv2.waitKey(1)
+    if k%256 == 27:
+        # ESC pressed
+        print("Escape hit, closing...")
+        break
+    elif k%256 == 32:
+        # SPACE pressed
+        img_name = "opencv_frame_{}.png".format(img_counter)
+        cv2.imwrite(img_name, frame)
+        print("{} written!".format(img_name))
+        img_counter += 1
+
+# Clean up
+video.release()
+cv2.destroyAllWindows()
+
+pt.pytesseract.tesseract_cmd = r'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
+file= 'opencv_frame_0.png'
+image= Image.open(file)
+
+# adding some sharpness and contrast to the image 
+enhancer1 = ImageEnhance.Sharpness(image)
+enhancer2 = ImageEnhance.Contrast(image)
+img_edit = enhancer1.enhance(20.0)
+img_edit = enhancer2.enhance(1.5)
+# save the new image
+img_edit.save("edited_image.png")
+
+text = pt.image_to_string(img_edit)
+with open('2.txt','w') as f:
+    f.write(text)
+    print("ready!")
+    
